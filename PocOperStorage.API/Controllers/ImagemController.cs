006b155093f2aa4caf846f9a9cd77d242375ea63
Murilo <murilo.gomes@agiltec.com>
2021-09-18 12:27:40 -0300

    Implementa envio de blob para container do azure.

---
+++ PocOperStorage.API/Controllers/ImagemController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using PocOperStorage.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PocOperStorage.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImagemController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ImagemController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(IFormFile file, [FromForm] Imagem command)
+        {
+            var imageUrl = await Upload(file);
+            command.UrlImagem = imageUrl;
+            return Ok(command);
+        }
+
+        private async Task<string> Upload(IFormFile file)
+        {
+            var accountName = _configuration["StorageConfiguration:AccountName"];
+            var accountKey = _configuration["StorageConfiguration:AccountKey"];
+            var containerName = _configuration["StorageConfiguration:ContainerName"];
+
+            var storageCredentials = new StorageCredentials(accountName, accountKey);
+            var storageAccount = new CloudStorageAccount(storageCredentials, true);
+            var blobAzure = storageAccount.CreateCloudBlobClient();
+            var container = blobAzure.GetContainerReference(containerName);
+
+            var blob = container.GetBlockBlobReference(file.FileName);
+            blob.Properties.ContentType = file.ContentType;
+            await blob.UploadFromStreamAsync(file.OpenReadStream());
+
+            return blob.SnapshotQualifiedStorageUri.PrimaryUri.ToString();
+        }
+    }
+}
---
+++ PocOperStorage.API/Models/Imagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PocOperStorage.API.Models
+{
+    public class Imagem
+    {
+        public string Nome { get; set; }
+        public string UrlImagem { get; set; }
+    }
+}
