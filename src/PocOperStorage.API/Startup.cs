915b26a28a32f8583d0ca4ac180a48f4ddf622bd
Murilo <murilo.gomes@agiltec.com>
2021-09-19 17:52:04 -0300

    add aplication insight

--- src/PocOperStorage.API/Startup.cs
+++ src/PocOperStorage.API/Startup.cs
@@ -46,6 +46,7 @@ namespace PocOperStorage.API
 
                 c.IncludeXmlComments(System.IO.Path.Combine(basePath, fileName));
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
