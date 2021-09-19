9b6fc1b0ee1822748d236714ab38068a9d89dcea
Murilo <murilo.gomes@agiltec.com>
2021-09-19 15:58:06 -0300

    test

--- src/PocOperStorage.API/Controllers/WeatherForecastController.cs
+++ src/PocOperStorage.API/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace PocOperStorage.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Col", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
